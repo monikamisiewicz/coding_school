20417d571ecbda951e0ab7bc66d6c533503efb64
Monika Misiewicz <zieba.monika@wp.pl>
2019-09-03 11:41:00 +0200

    update Main class

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,4 +1,41 @@
 package pl.coderslab;
 
+import pl.coderslab.dao.ExerciseDao;
+import pl.coderslab.dao.SolutionDao;
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.dao.UserGroupDao;
+import pl.coderslab.management.*;
+import pl.coderslab.model.User;
+import pl.coderslab.model.UserGroup;
+
+
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        UserDao userDao = new UserDao();
+        UserGroupDao userGroupDao = new UserGroupDao();
+        ExerciseDao exerciseDao = new ExerciseDao();
+        SolutionDao solutionDao = new SolutionDao();
+
+//        ManageUser users = new ManageUser();
+//        users.manageUser();
+
+//        ManageGroup groups = new ManageGroup();
+//        groups.manageUserGroup();
+
+//        ManageExercise exercises = new ManageExercise();
+//        exercises.manageExercise();
+
+//        ManageSolution solutions = new ManageSolution();
+//        solutions.manageSolution();
+
+
+        User user = new User();
+        UserProgram userProgram = new UserProgram(user);
+        userProgram.options();
+
+    }
 }
