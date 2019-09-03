d3075401a1e36bca82835137b020151b733cd9f1
Monika Misiewicz <zieba.monika@wp.pl>
2019-09-03 11:39:54 +0200

    updated utils package

--- src/pl/coderslab/utils/DBUtil.java
+++ src/pl/coderslab/utils/DBUtil.java
@@ -1,4 +1,19 @@
 package pl.coderslab.utils;
 
-public class DBUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBUtil {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/coding_school?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "coderslab";
+
+    private DBUtil () {}
+
+    public static Connection createConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+
 }
